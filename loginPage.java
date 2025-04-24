b6353d558366c84bb7480d1fb4215b2093735d21
lorenzooo <marklaurence355@gmail.com>
2025-04-24 16:53:32 +0800

    First Commit Setting up the frame in swing

---
+++ loginPage.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class loginPage {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Bark & Meow Essentials - Login");
+        frame.setResizable(false);
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.setVisible(true);
+    }
+}
